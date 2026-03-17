b9e3882fb2cbe6e655662cce5bb39d0cddc6b493
Nitesh <paswannitesh773@gmail.com>
2026-03-18 00:34:07 +0530

    firstupload

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/go',
+        destination: 'https://www.amazon.in/dp/ASIN?tag=yourtag-21',
+        permanent: false, // Use false (302) so you can change the link later
+      },
+      {
+        source: '/go/flipkart-deal',
+        destination: 'https://www.flipkart.com/product-link?affid=yourid',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
